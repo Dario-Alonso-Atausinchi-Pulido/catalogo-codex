39156a5d17fba53d635db8dcea82ad92de8fbd8a
dario.atasunchip <dalonsoatausinchip@gmail.com>
2026-03-27 14:24:11 -0500

    feat: eliminar módulos de juegos demo y actualizar favicon a SVG

--- src/app/games/library/game-library.routes.ts
+++ src/app/games/library/game-library.routes.ts
@@ -1,25 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const gameLibraryRoutes: Routes = [
-  {
-    path: 'games/demo-runner',
-    loadComponent: () =>
-      import('./demo-runner/demo-runner-page.component').then(
-        (module) => module.DemoRunnerPageComponent
-      )
-  },
-  {
-    path: 'games/neon-paddle',
-    loadComponent: () =>
-      import('./neon-paddle/neon-paddle-page.component').then(
-        (module) => module.NeonPaddlePageComponent
-      )
-  },
-  {
-    path: 'games/pixel-jump',
-    loadComponent: () =>
-      import('./pixel-jump/pixel-jump-page.component').then(
-        (module) => module.PixelJumpPageComponent
-      )
-  }
-];
+export const gameLibraryRoutes: Routes = [];
